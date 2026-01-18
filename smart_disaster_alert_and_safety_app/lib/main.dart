4d0f9b98cc574a02201a608b9889d0c723546fd9
Sohag <ahadulsohag@gmail.com>
2026-01-18 22:28:06 +0600

    Update main.dart

--- smart_disaster_alert_and_safety_app/lib/main.dart
+++ smart_disaster_alert_and_safety_app/lib/main.dart
@@ -4,15 +4,40 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
       home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('data')),
+          backgroundColor: Colors.amberAccent,
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: ElevatedButton(onPressed: () {}, child: Text('Click')),
+        ),
+        bottomNavigationBar: Container(
+          height: 60,
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [],
+          ),
         ),
       ),
     );
